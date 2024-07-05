33b0b2cbf6d219a5abb25ca1a3ddf424a82c2084
ito25 <nagoyashoka25ito@gmail.com>
2024-07-05 11:35:36 +0900

    [Add] Webアプリケーションを作ってみよう-2

--- omikuji.js
+++ omikuji.js
@@ -1,11 +1,13 @@
-et name;
+
 let username;
 let userresult;
+username = prompt("お名前を教えて下さい。");
 
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
 
-let rand = Math.floor( Math.random() * 5);
+
+let rand = Math.floor( Math.random() * 5) ;
 if (rand == 0) {
   userresult = "大吉";
 }
